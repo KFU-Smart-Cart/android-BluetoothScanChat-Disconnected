8a5853f4b7b93227ca7829915e34c6c7f9947296
Mohammed Al-Qahtani <mx9111@gmail.com>
2015-10-17 17:25:36 +0300

    Initial commit

---
+++ Application/src/main/java/com/example/android/bluetoothchat/ScanActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.bluetoothchat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ScanActivity extends Activity {
+
+    IntentIntegrator integrator = new IntentIntegrator(ScanActivity.this);
+    String text = "" ;
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        integrator.addExtra("SCAN_WIDTH", 640);
+        integrator.addExtra("SCAN_HEIGHT", 480);
+        integrator.addExtra("SCAN_MODE", "QR_CODE_MODE");
+        integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+        integrator.addExtra("SAVE_HISTORY", false);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) {
+            text = result.getContents();
+//            Intent returnIntent = new Intent();
+//            returnIntent.putExtra("text",text);
+//            setResult(RESULT_OK,returnIntent);
+//            finish();
+
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra(EXTRA_DEVICE_ADDRESS, text);
+
+            // Set result and finish this Activity
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        }
+    }
+}
